55ed028fbcab24ae7d9602f4e09abf97c2c3ca97
unknown <aida.pirusic@hotmail.com>
2019-09-21 22:34:02 +0200

    Dropdown added on toolbar.

--- src/app/components/Toolbar/toolbar.component.ts
+++ src/app/components/Toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { OnInit, Component } from '@angular/core';
 import { UsersService } from 'src/app/services/UsersService';
 import { Router } from '@angular/router';
+import { TranslateService } from 'src/app/services/translate';
 
 @Component({
     selector: 'toolbar',
@@ -10,15 +11,20 @@ import { Router } from '@angular/router';
 
 export class ToolbarComponent implements OnInit {
     username: string;
+    showDropdown: boolean = false;
 
-    constructor(private _usersService: UsersService, private _router: Router) {
-        if (!_usersService.isLogged()){
+    constructor(private _usersService: UsersService, private _router: Router, private _translateService: TranslateService) {
+        if (!_usersService.isLogged()) {
             this._router.navigate(['/login']);
         }
     }
 
     ngOnInit(): void {
         this.username = this._usersService.getUsername();
+    }
+
+    onShowDropdownClicked() {
+        this.showDropdown = !this.showDropdown;
     }
 
     onLogoutClicked() {
--- src/app/services/translate/lang-en.ts
+++ src/app/services/translate/lang-en.ts
@@ -6,5 +6,6 @@ export const LANG_EN_TRANS = {
     '_REMEMBER_ME': 'Remember me',
     '_FORGET_PASSWORD': 'Forget your password?',
     '_SIGN_IN': 'Sign in',
-    '_USER_DATA_NOT_VALID':'User data is not valid.'
+    '_USER_DATA_NOT_VALID':'User data is not valid.',
+    '_LOGOUT':'Log out'
 };
